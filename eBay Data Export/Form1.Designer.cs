43f9c8c7f65b9f8fb66949a9696a481568dcdfd2
DESKTOP-H33JJKE\EthanOllins <ethanollins6@gmail.com>
2017-10-28 16:09:56 -0700

    Added GetSessionID call

--- eBay Data Export/Form1.Designer.cs
+++ eBay Data Export/Form1.Designer.cs
@@ -28,14 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 261);
+            this.ClientSize = new System.Drawing.Size(984, 502);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -44,6 +57,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- eBay Data Export/Form1.cs
+++ eBay Data Export/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using eBay.Service.Core.Sdk;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -11,6 +13,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace eBay_Data_Export
 {
@@ -21,21 +24,33 @@ namespace eBay_Data_Export
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.sandbox.ebay.com/ws/api.dll/");
-            request.Method = "POST";
-            request.Headers.Add("X-EBAY-API-DEV-NAME", "GregoryM-mailer-SBX-b45f0c763-f90c92ad");
-            request.Headers.Add("X-EBAY-API-APP-NAME", "8105fd0e-a76c-4e10-80e8-43e86ab59f7c");
-            request.Headers.Add("X-EBAY-API-CERT-NAME", "SBX-45f0c76378c2-8dd7-40e6-b49d-0c12");
-            request.Headers.Add("X-EBAY-API-COMPATIBILITY-LEVEL", "967");
-            request.Headers.Add("X-EBAY-API-CALL-NAME", "GetSessionID");
-            request.Headers.Add("X-EBAY-API-SITEID", "0");
-            //request.Headers.Add("X-EBAY-API-IAF-TOKEN", "v^1.1#i^1#f^0#I^3#r^0#p^3#t^H4sIAAAAAAAAAOVXW2wUVRhmezMVWh5shIjGdQqGQmb3zM7s7MyE3bptQZb0yhaECilzOVMGZmfWOWfaLhfTNNI0CEJJTDRRqIRIAhjjiyJgQoiIGh94MF5CxASIMQ2iAqYPinpme2FbI/RCYhPnZXLO+W/f/3//uYCuouIlPSt7Bkt8D+X1d4GuPJ+PmQ2KiwqXlubnPVY4C+QI+Pq7FnYVdOf/uAzJKTMtrYYobVsI+jtTpoWk7GSUch1LsmVkIMmSUxBJWJWS8bpaKRQAUtqxsa3aJuVP1EQpOaxDTYOawPAsz4MQmbVGbDbbUUpjQgoI8wJktbAI1TBZR8iFCQth2cJRKgSYCM0AOiQ0g7DECRInBsJspIXyr4UOMmyLiAQAFcuGK2V1nZxY7x2qjBB0MDFCxRLxFcmGeKJmeX3zsmCOrdhwHpJYxi4aO6q2NehfK5suvLcblJWWkq6qQoSoYGzIw1ijUnwkmCmEn001qwiaIoZ1QRD4CKPwDySVK2wnJeN7x+HNGBqtZ0UlaGEDZ+6XUZINZQtU8fConphI1Pi9X5Mrm4ZuQCdKLa+Kr1+TXL6a8icbGx273SBE8pAyLMuHOI4LUTEMEUkhdFqR7KpQl1VsO5lhf0NGh7M9zmG1bWmGlzvkr7dxFSTBw/EpYnNSRIQarAYnrmMvsFy5yGgqmRavtkPFdPFmyysvTJF8+LPD+xdihBl3ufCguKGIOuBZgbRhiAHhXG54vT51fsS8EsUbG4NeLFCRM3RKdrZCnDZlFdIqSa+bgo6hSWxYD7GCDmmNF3WaE3WdVsIaTzM6hABCRVFF4X9IE4wdQ3ExHKXK+IUs1iiVVO00bLRNQ81Q40WyO9AwMTpRlNqMcVoKBjs6OgIdbMB22oIhAJjgurrapLoZpmRqVNa4vzBtZFmrQqKFDAln0iSaTsJA4txqo2KsozXKDs5UuRkyTkLTJL8RFo+JMDZ+9l+gIg/qzALp6SNiQE4bAY/kAdVOBW2ZNLU31ZqN2D8RoaDiZoh/DToBB8qabZmZieu1uYTEQ9oTU0KkGoGhfiQwiEev1yfjdayBSegYVjvhMmmxScIcqzwJHVlVbdfCU3E3rDoJDd01dcM0vXadisMc9cmEaclmBhsqGnU5rS6Lp9MJbWZ12bMObCNVryNHiGFCh05WraMVLqwDNcKztC4CVQzJ2rRQa7DdUGGrMcOQW65pTgtXDWwfX03S60f/a1wCuWvoGoC0HOFVmoPkKBQAFGiOhQIvK2FRj6jTwl3XNtNKWR+MTwtRtWmQfaE5M9OOwJU2wnB6zVdNLqQzC5S3w4xsMBFBDdGCpkVoDkCebDyiRgOVCU0U8riJnAvdP670wbFP69is7Md0+z4C3b4PyescRADNLAUVRflrCvLnUMjAMIBkS1PszoAh6wFktFnk5ejAwFaYScuGk1fke37BQOUfOY/6/o1g/uizvjifmZ3zxgeP310pZObOK2EiDAgJIMwJnNgCyu+uFjCPFpTt3XC89Ok+tvXIzwLuqvhl8ZyFvjAoGRXy+QpnFXT7Zp05+P7HgyVfPncovu+zM1LRoj+bt5X/2iEOtMz3XS3df+XavhN3bmQWzKG2N5Vf3H2nqvn3ge+eeaftyskVl8/e6t15qaK074Puha/1Xbt5bOcXB07eXsKVHE7c3lH4w9HjPa9s/PQT7s23XvjKv+2b3t1PbShrOnt+zxKudc+FHfuv/vXG+VP8pejgb6veTpw7KO4Vzn0vLdhlnN50g08c8TWdOPDI58Krx9kXj/b3GvPjqQsdFwcHXjp1M3B91eH1p/e+i/qaXr9c7lyfu2ht5ROHv9205eAVn3KmZ9fDePGTUt+q/O2VDT/1i9GXv9ZDvcdurauodHfUNpYNlEnce7Ud57bwh/w3T8wrtk6eHirj3wO7mn9uEQAA");
-            request.ContentType = "text/xml";
+            
+        }
 
-            string postData = "<?xml version=\"1.0\" encoding=\"utf-8\"?><GetSessionIDRequest xmlns=\"urn:ebay:apis:eBLBaseComponents\"><ErrorLanguage>en_US</ErrorLanguage><WarningLevel>High</WarningLevel><RuName>Gregory_Morris-GregoryM-mailer-fkfdcrz</RuName></GetSessionIDRequest>";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://api.sandbox.ebay.com/ws/api.dll");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("x-ebay-api-siteid", "0");
+            request.AddHeader("x-ebay-api-call-name", "GetSessionID");
+            request.AddHeader("x-ebay-api-compatibility-level", "967");
+            request.AddHeader("content-type", "text/xml");
+            request.AddHeader("x-ebay-api-cert-name", "SBX-45f0c76378c2-8dd7-40e6-b49d-0c12");
+            request.AddHeader("x-ebay-api-dev-name", "8105fd0e-a76c-4e10-80e8-43e86ab59f7c");
+            request.AddHeader("x-ebay-api-app-name", "GregoryM-mailer-SBX-b45f0c763-f90c92ad");
+            request.AddParameter("text/xml", "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<GetSessionIDRequest xmlns=\"urn:ebay:apis:eBLBaseComponents\">\n\t<RuName>Gregory_Morris_-GregoryM-mailer-fkfdcrz</RuName>\n\t<ErrorLanguage>en_US</ErrorLanguage>\n\t<MessageID></MessageID>\n\t<Version>967</Version>\n\t<WarningLevel>High</WarningLevel>\n</GetSessionIDRequest>", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            //MessageBox.Show(((int)response.StatusCode).ToString());
+            //MessageBox.Show(response.Content);
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(response.Content);
+            XmlNodeList nodes = ((XmlElement)doc.GetElementsByTagName("GetSessionIDResponse")[0]).GetElementsByTagName("SessionID");
+
+            string sessionID = nodes[0].InnerText;
         }
     }
 }
